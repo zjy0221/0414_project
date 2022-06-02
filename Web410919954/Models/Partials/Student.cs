7cf3b1ed4ebda76ba1f9df6c38cd82801faaa26b
莊婕榆 <s1091995@gm.pu.edu.tw>
2022-06-02 20:41:16 +0800

    0602更新，學生資料庫

---
+++ Web410919954/Models/Partials/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web410919954.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+
+    }
+
+    public class StudentMetadata
+    {
+        public int Id { get; set; }
+        //
+        //
+        [Required(ErrorMessage = "請填寫姓名")]
+        [Display(Name = "姓名")]
+        [MinLength(2, ErrorMessage = "請輸入2 - 5個字")]
+        [MaxLength(5, ErrorMessage = "請輸入2 - 5個字")]
+        public string Name { get; set; }
+        //
+        //
+        [Required(ErrorMessage = "請填寫學號")]
+        [Display(Name = "學號")]
+        [StringLength (10,  MinimumLength =1 ,  ErrorMessage = "請輸入1 - 10個字")]
+        public string Number { get; set; }
+        //
+        //
+        [Required(ErrorMessage = "請填寫 E-mail")]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+
+
+
+
+}
